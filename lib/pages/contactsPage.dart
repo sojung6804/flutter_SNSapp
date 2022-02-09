0e5269463c86be0334912facdcf8e80e3f6bb6c5
sopy <gorhtlfj2835@gmail.com>
2022-02-09 14:59:51 +0900

    9.2 Firbas docs 접근

--- lib/pages/contactsPage.dart
+++ lib/pages/contactsPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final firestore = FirebaseFirestore.instance;
 
 class contactsPage extends StatefulWidget {
   const contactsPage({Key? key}) : super(key: key);
@@ -8,6 +11,19 @@ class contactsPage extends StatefulWidget {
 }
 
 class _contactsPageState extends State<contactsPage> {
+  getData() async {
+    var result = await firestore.collection('product').get();
+    for (var fb in result.docs) {
+      print(fb['name']);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
